cb8587db1d80104d0c0204d60a8b3c0242f94d87
Vadzim Hulkovich 66745 <99969973+Vadzim-Hulkovich-66745@users.noreply.github.com>
2026-04-13 13:43:24 +0200

    zadanie 5 Vadzim Hulkovich 66745

--- script.js
+++ script.js
@@ -24,3 +24,72 @@ toggleSkillsBtn.addEventListener("click", () => {
         skills.style.display = "none";
     }
 });
+
+// ===== ZADANIE 5 - WALIDACJA =====
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // поля
+    const firstName = document.getElementById("firstName").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    // ошибки (div)
+    const firstNameError = document.getElementById("firstNameError");
+    const lastNameError = document.getElementById("lastNameError");
+    const emailError = document.getElementById("emailError");
+    const messageError = document.getElementById("messageError");
+
+    // очистка
+    firstNameError.textContent = "";
+    lastNameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent = "";
+
+    let isValid = true;
+
+    // === WALIDACJA IMIĘ ===
+    if (firstName === "") {
+        firstNameError.textContent = "Imię jest wymagane";
+        isValid = false;
+    } else if (/\d/.test(firstName)) {
+        firstNameError.textContent = "Imię nie może zawierać cyfr";
+        isValid = false;
+    }
+
+    // === NAZWISKO ===
+    if (lastName === "") {
+        lastNameError.textContent = "Nazwisko jest wymagane";
+        isValid = false;
+    } else if (/\d/.test(lastName)) {
+        lastNameError.textContent = "Nazwisko nie może zawierać cyfr";
+        isValid = false;
+    }
+
+    // === EMAIL ===
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email === "") {
+        emailError.textContent = "Email jest wymagany";
+        isValid = false;
+    } else if (!emailPattern.test(email)) {
+        emailError.textContent = "Niepoprawny email";
+        isValid = false;
+    }
+
+    // === MESSAGE ===
+    if (message === "") {
+        messageError.textContent = "Wiadomość jest wymagana";
+        isValid = false;
+    }
+
+    // SUCCESS
+    if (isValid) {
+        alert("Formularz wysłany poprawnie!");
+        form.reset();
+    }
+});
